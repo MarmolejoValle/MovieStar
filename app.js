2c8bbe21a94e21f4718f8226aa45815e384021e5
AlbertFullPowerS <133397938+AlbertFullPowerS@users.noreply.github.com>
2024-11-12 18:47:23 -0600

    configuracion de express

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const port = 2003;
+const url = "localhost"
+const app = express();
+
+app.set('trust proxy', true);
+app.use(cors({ origin: '*' }));
+app.use(bodyParser.json());
+
+app.listen(port, function() { 
+    console.log("Servidor iniciado");
+    });
