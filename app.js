69128166cdc0b800e5e896070ccac5a30a883732
AlbertFullPowerS <133397938+AlbertFullPowerS@users.noreply.github.com>
2024-11-24 01:02:52 -0600

    Automatizacion de rutas

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express();
 app.set('trust proxy', true);
 app.use(cors({ origin: '*' }));
 app.use(bodyParser.json());
+app.use('/api', require('./router'));
 
 app.listen(port, function() { 
     console.log("Servidor iniciado");
