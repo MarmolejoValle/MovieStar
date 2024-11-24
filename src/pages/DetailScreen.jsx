df503f20faacb0fba7ab6cb5de5f9a2086e0e642
AlanDaniel <20223TN018@UTEZ.EDU.MX>
2024-11-24 11:18:20 -0600

    Se agrega el componente iframe para la reproducción en la pagina de detalles ya funcional(falta moverlo)

--- src/pages/DetailScreen.jsx
+++ src/pages/DetailScreen.jsx
@@ -52,6 +52,19 @@ const DetailScreen = () => {
           </div>
         </div>
       </div>
+
+      {/* Fragmento sacado de yt API: trailer de la pelicula */}
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/mXd1zTwcQ18?si=uF3feHVSf02FEMbL"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen>
+      </iframe>
+
       <Footer />
     </div>
   );
