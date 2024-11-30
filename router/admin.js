adcf240ff1c6d4b68a7988dbd57583c7b35b63b3
AlbertFullPowerS <133397938+AlbertFullPowerS@users.noreply.github.com>
2024-11-30 15:30:03 -0600

    comentarios

--- router/admin.js
+++ router/admin.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const { postaddPromotion ,getviewAllPromotions,postdeletePromotion} = require('../controller/promotion');
 
+//EndPoint para la gestion de promociones
 router.post('/promotion/add', postaddPromotion);
 router.get('/promotion/viewAll', getviewAllPromotions);
 router.post('/promotion/delete', postdeletePromotion);
