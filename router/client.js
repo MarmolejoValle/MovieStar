caa1bd0cc9eb41ca5e1ff5bcccceea4c97837dc1
AlbertFullPowerS <133397938+AlbertFullPowerS@users.noreply.github.com>
2024-12-01 16:04:32 -0600

    Transpaso de endPoint de ventas

--- router/client.js
+++ router/client.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const router = express.Router();
 
-const {postAddSale,postCreateAccount,postLibrary} = require('../controller/client');
+const {postCreateAccount,postLibrary} = require('../controller/client');
 
 router.post('/library', postLibrary);
-router.post('/addSale', postAddSale);
 router.post('/createAccount',postCreateAccount);
 
 module.exports = router
